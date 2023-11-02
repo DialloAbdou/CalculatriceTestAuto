77d97e97e35dbc20906edc3b06b24110e44eaa72
= <=dialloabdou@hotmail.com>
2023-11-02 13:21:52 +0100

    Fondamentaux Test Automatiser

---
+++ Caculate/src/services/Calcul/CalculService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcul
+{
+    public class CalculService
+    {
+        /// <summary>
+        /// elle retourne la somme deux valeurs
+        /// /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// elle fait la soustraction
+        /// des deux valeurs
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int soustration(int a, int b)
+        {
+            return (a - b);
+        }
+
+        /// <summary>
+        ///  elle permet de multiplication
+        ///  entre deuw valeurs
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Mulitiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        // elle permet de faire la division
+        // entre deux valeurs
+        /// </summary>
+        /// <param name="a"></param> 
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public int division(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Interdir de divser un nombre par Zero");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Caculate/tests/services/CalculTest/CalculServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using Calcul;
+using DotNetty.Transport.Channels;
+
+namespace CalculTest
+{
+    public class CalculServiceTest
+    {
+
+        private CalculService service;
+
+        public CalculServiceTest()
+        {
+            service = new CalculService();
+        }
+        [Theory]
+        [InlineData(5, 5, 10)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-10, -20, -30)]
+
+        public void additon_should_return_Value_After_Sum_Two_Parametre_Passed(int a, int b, int expected)
+        {
+            // Arrange => preparer mon environnement de travail
+
+            // Action
+            int resultat = service.addition(a, b);
+            // Assert
+            Assert.Equal(expected, resultat);
+        }
+
+
+        [Theory]
+        [InlineData(5, 5, 0)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-10, -20, 10)]
+        public void soustration_should_return_value_after_subtraction_two_parameters(int a, int b, int expected)
+        {
+            // Arrange => preparer mon environnement de travail
+            // Action
+            var resultat = service.soustration(a, b);
+            // Assert
+            Assert.Equal(expected, resultat);
+        }
+
+
+        [Theory]
+        [InlineData(5, 5, 25)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-10, -20, 200)]
+        public void Multiplication_should_return_value_after_subtraction_two_parameters(int a, int b, int expected)
+        {
+            // Arrange => preparer mon environnement de travail
+            // Action
+            var resultat = service.Mulitiplication(a, b);
+            // Assert
+            Assert.Equal(expected, resultat);
+        }
+
+
+        [Theory]
+        [InlineData(5, 5, 1)]
+        [InlineData(20, 5, 4)]
+        public void division_should_return_value_after_divided_two_parameters(int a, int b, int expected)
+        {
+            // Arrange => preparer mon environnement de travail
+            // Action
+            var resultat = service.division(a, b);
+            // Assert
+            Assert.Equal(expected, resultat);
+        }
+
+        [Fact]
+        public void division_should_ThrowArgumentExeception_When_second_param_equalTo_Zero()
+        {
+            // Arrange => preparer mon environnement de travail
+            // Action
+            Assert.Throws<DivideByZeroException>(() => service.division(10, 0));
+            // Assert
+
+        }
+    }
+}
